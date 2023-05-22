49ef08bc890b82cb60f9056eef88a095b58244ef
Bro74 <bmwhite1014@gmail.com>
2023-05-21 21:08:39 -0400

    reworked on questions and .then repsonse

--- index.js
+++ index.js
@@ -16,24 +16,29 @@ inquirer
 .prompt ([
     {
         type: 'maxlength-input',
-        message: 'Start creating your SVG logo! Please enter your text, only 3 characters are permitted.',
+        message: "Let's create an SVG logo! Enter your text, 3 character limit.",
         name: 'text',
         maxLength: 3,
     },
     {
         type: 'input',
-        message: 'What color would you like your text? Enter a color keyword or hexidecimal value.',
+        message: 'What color is the text? Enter a color keyword or hexidecimal value.',
         name:'textColor',
     },
     {
         type: 'list',
-        message: 'What shape would you like your logo to be?',
+        message: 'What shape is the logo?',
         name: 'shape',
         choices: ['Circle', 'Triangle', 'Square'],
 
     },
     {
         type: 'input',
-        message: 'What color would you like your shape? Enter a color keyword or hexidecimal value.',
+        message: 'What color is the shape? Enter a color keyword or hexidecimal value.',
         name: 'shapeColor',
     }
+
+]).then((response) =>
+writeSVG('logo.svg', response)
+
+);
